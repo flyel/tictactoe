87575ec3277f1c7b085ce3419cdb4068968d602e
xehivs <pawel@ksienie.com>
2015-05-29 11:17:45 +0200

    Git init

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	clear
+	g++ *.cpp -o tictactoe
+	./tictactoe
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <time.h>
+#include "Board.h"
+
+int main(int argc, const char * argv[]) 
+{	
+	srand((unsigned int)time(NULL));
+	
+	Board board;
+
+	for (int i = 0; i < 2; i++)
+	{
+		while(!board.play(rand() % 9, i % 2 ? cross : circle));
+		board.show();
+		printf("\n\n");
+	}
+	
+    return 0;
+}
