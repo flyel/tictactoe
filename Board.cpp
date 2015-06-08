9f7afc40feddd547ebcdda5c416d83afc8e1ce6b
flyel <219099@student.pwr.edu.pl>
2015-06-08 07:44:53 -0700

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -8,18 +8,30 @@ Board::Board()
 	currentSubgrid = 0;
 }
 
+
 void Board::show()
 {
-	int a = -1, b = 9;
-	for (int i=0 ; i<81 ; i++, b++)
-	{
-		if (!( i % 3))	{ printf(" ");	a++;	b-=3; }
-		if (!( i % 9))	{ printf("\n"); a-=3;	b+=3; }
-		if (!( i % 27)) { printf("\n"); a+=3;	b-=9; }
+	   int a=-1, b=9;
+	   printf("\n");
+	   printf("\n");
+    for(int i=0; i<81; i++, b++)
+    {
+        if(i!=0 && !(i%3)) {printf(" |   "); a++; b-=3; }
+        if(i!=0 && !(i%9)) {printf("\n\n"); a-=3; b+=3; }
 
-		printf("%c", grid[a][b]);
-	}
+        if(i!=0 && !(i%27)) {printf(" ");
+        for(int p=0; p<47; p++)
+            {printf("-");}
+        printf("\n\n"); a=3; b=0;}
+        if(i!=0 && !(i%54)) {a=6; b=0;};
+        if(!(i%1)) {printf(" | ");}
+        printf("%c", grid[a][b]);
+        if(i==80) {printf(" | ");}
+
+
+    }
 }
+
 
 bool Board::play(int space, enum figures figure)
 {
